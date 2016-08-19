4bfbda6f50c13bbd5ffe6d57a2be33c088d963b9
Max Shen <maxwshen@gmail.com>
2016-08-19 21:44:37 +0200

    fix negative gaps after assebly refinement

--- ragout/assembly_graph/assembly_refine.py
+++ ragout/assembly_graph/assembly_refine.py
@@ -90,16 +90,18 @@ def _insert_from_graph(graph, scaffolds_in, max_path_len, contigs_fasta):
 
             #insert contigs along the path
             supp_genomes = prev_cont.link.supporting_genomes
+            prev_cont.link.supporting_assembly = True
+            prev_cont.link.gap = config.vals["min_scaffold_gap"]
             for node in path_nodes:
                 sign = 1 if node[0] == "+" else -1
                 name = node[1:]
 
-                seq_len = len(contigs_fasta[name])
-                new_scaffolds[-1].contigs.append(Contig.with_sequence(name, seq_len, sign))
-                new_scaffolds[-1].contigs[-2].link.supporting_assembly = True
-                new_scaffolds[-1].contigs[-1].link.supporting_assembly = True
-                new_scaffolds[-1].contigs[-1].link.supporting_genomes = \
-                                                                supp_genomes
+                new_contig = Contig.with_sequence(name, 
+                                    len(contigs_fasta[name]), sign)
+                new_contig.link.supporting_assembly = True
+                new_contig.link.gap = config.vals["min_scaffold_gap"]
+                new_contig.link.supporting_genomes = supp_genomes
+                new_scaffolds[-1].contigs.append(new_contig)
 
         new_scaffolds[-1].contigs.append(scf.contigs[-1])
 
